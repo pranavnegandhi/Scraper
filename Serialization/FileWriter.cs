82d8f631e0fc9edbcfb8e1cb443fc2bf1b590d30
Pranav Negandhi <pranav@vertika.tv>
2020-08-11 21:29:01 +0530

    Implemented a FileWriter class to write a stream to any file on the hard disk

---
+++ Serialization/FileWriter.cs
@@ -0,0 +1,38 @@
+﻿using Notadesigner.Scraper.Serialization;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Scraper.Serialization
+{
+    public class FileWriter
+    {
+        private readonly IFileLayout _layout;
+
+        public FileWriter(IFileLayout layout)
+        {
+            _layout = layout;
+        }
+
+        public async Task WriteAsync(Stream inputStream, string name)
+        {
+            try
+            {
+                var outputPath = _layout.GetPath(name);
+                using (var outputStream = File.Create(outputPath.FullName))
+                {
+                    using (var writer = new StreamWriter(outputStream))
+                    {
+                        await inputStream.CopyToAsync(outputStream);
+                        await writer.FlushAsync();
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                Trace.TraceError(exception.Message);
+            }
+        }
+    }
+}
