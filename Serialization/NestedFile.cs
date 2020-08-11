f6655fb6c0d611288955e82268f776bf603bc742
Pranav Negandhi <pranav@vertika.tv>
2020-08-11 21:28:13 +0530

    Added a NestedFile layout class that can be used to download a file to an arbitrarily deeply nested directory

---
+++ Serialization/NestedFile.cs
@@ -0,0 +1,34 @@
+﻿using Notadesigner.Scraper.Serialization;
+using System.IO;
+
+namespace Scraper.Serialization
+{
+    class NestedFile : IFileLayout
+    {
+        private readonly DirectoryInfo _basePath;
+
+        public NestedFile(string basePath)
+        {
+            _basePath = new DirectoryInfo(basePath);
+            if (!_basePath.Exists)
+            {
+                _basePath.Create();
+            }
+        }
+
+        public FileInfo GetPath(string name)
+        {
+            var fileName = Path.GetFileName(name);
+            var relativePath = Path.GetDirectoryName(name).Trim(Path.DirectorySeparatorChar);
+            var filePath = Path.Combine(_basePath.FullName, relativePath, fileName);
+            var info = new FileInfo(filePath);
+            var dir = info.Directory;
+            if (!dir.Exists)
+            {
+                dir.Create();
+            }
+
+            return info;
+        }
+    }
+}
